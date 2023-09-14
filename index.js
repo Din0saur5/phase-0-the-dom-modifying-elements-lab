23ea16eb71bf265b60a74d71a0f05fc41a8ce6de
Dan Hackworth <danielhackworth515@gmail.com>
2023-09-14 17:05:53 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+
+const main = document.querySelector("main#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Dan is the champion";
+//took so long because i kept trying to append first
